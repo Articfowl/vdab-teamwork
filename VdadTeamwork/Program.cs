cd2facba82306f6c67b754e5ee22aa94a502f3c9
Gus Vermeulen <gus.v@live.be>
2019-07-08 12:18:08 +0200

    Test commit

--- VdadTeamwork/Program.cs
+++ VdadTeamwork/Program.cs
@@ -7,6 +7,7 @@ namespace VdadTeamwork
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
